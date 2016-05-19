2338a25b14a9a612a0cc39a9e074a71953908da2
MADDILLA <MADDILLA@Matisses-MacBook-Air.local>
2016-05-19 16:19:22 -0700

    Testimonials fix after loading other page and returning to hoe

--- wp-content/themes/angular-spa/assets/js/paramountDefault.js
+++ wp-content/themes/angular-spa/assets/js/paramountDefault.js
@@ -2,7 +2,6 @@ var paramountDefault = (function($) {
 	'use strict';
 	var _private = {
 		_run: function(el, val) {
-
 			var od = new Odometer({
 				  el: el,
 				  value: 0,
@@ -12,7 +11,6 @@ var paramountDefault = (function($) {
 				  theme: 'plaza',
 				  duration: 3000
 			});
-
 			od.update(val);
 		},
 		_cache: {
@@ -26,17 +24,10 @@ var paramountDefault = (function($) {
 				slidesToShow: 2,
   				slidesToScroll: 1
 			});
-
-
 			if ($('.customer-service').length) {
 				this._customerService();
 			}
-
 			this._bind();
-
-
-
-
 		},
 		_cache: {
 			//testimonialsData: paramountDefault._trustPilotData(),
@@ -45,26 +36,29 @@ var paramountDefault = (function($) {
 		_bind: function() {
 			var firstCall = true;
 			$(document).ready(function() {
-
 // $('#mobileNavHam').on('click', function(e) {
 // 	console.log('clicked');
 //     $(this).toggleClass('fa-times');
 // });
-
 				setTimeout(function(){
-						var testimonialsData = _private._cache.testimonialsObj;
-						testimonialsData = paramountDefault._trustPilotData('', false, firstCall);
-						var dataString = JSON.stringify(testimonialsData);
-						console.log(testimonialsData);
-						$('#testimonialsData').val(dataString);
+					var testimonialsData = _private._cache.testimonialsObj;
+					testimonialsData = paramountDefault._trustPilotData('', false, firstCall);
+					var dataString = JSON.stringify(testimonialsData);
+					//console.log(testimonialsData);
+					$('#testimonialsData').val(dataString);
 
+				}, 1000);
+
+				$('.logo-link').on('click', function () {
+					setTimeout(function(){
+						paramountDefault._homeClick();
 					}, 1000);
+				});
 
+				$('nav li a').on('click', function () {
+					$('.testimonials-slider').slick('unslick');
+				});22
 			});
-
-
-			// trustpilot = {};
-
 
 			// XHR listener
 			// re initializes js after angular calls
@@ -76,20 +70,6 @@ var paramountDefault = (function($) {
 			  // this.url    :the url of the requested script (including query string, if any) (urlencoded)
 			  // this.data   :the data sent, if any ex: foo=bar&a=b (urlencoded)
 			  setTimeout(function(firstCall){
-
-
-			  	if ($('.testimonials-slider').length) {
-					// setTimeout(function(){
-						var data = $('#testimonialsData').val();
-
-
-						data = JSON.parse(data);
-						console.log(data);
-						paramountDefault._trustPilot(data, false, firstCall);
-						//paramountDefault._testimonials();
-					// }, 1000);
-				}
-
 			  	if ($('.customer-service').length) {
 			  		paramountDefault._customerService();
 			  	}
@@ -120,6 +100,14 @@ var paramountDefault = (function($) {
 			}
 
 		},
+		_homeClick: function() {
+			var data = $('#testimonialsData').val();
+			data = JSON.parse(data);
+			console.log(data);
+			var firstCall = true;
+			paramountDefault._trustPilot(data, true, firstCall);
+			console.log('called after _homeClick');
+		},
 		_compileAngularElement: function( el ) {
 			var el = (typeof el == 'string') ? el : null ;
 			// The new element to be added
@@ -140,64 +128,35 @@ var paramountDefault = (function($) {
 			this._testimonials();
 		},
 		_testimonials: function(testimonialsData) {
-			console.log(testimonialsData);
-			//if($('.testimonials-slider').length) {
-			//this._trustPilot();
-			//
-			//
-			// INITIALIZES SLICK PLUGIN
-			// REQUIQUIRES JSON DATA TO BE SET AND CONTENT APPENDED INTO 'slick-slider fdiv'
-				$('.testimonials-slider').slick({
-					slidesToShow: 1,
-	  				slidesToScroll: 1,
-					arrows: false,
-					autoplay: true
-				});
-				console.log('testimonial')
-			//}
+			// $('.testimonials-slider').on('init', function(event, slick, currentSlide, nextSlide){
+			// 	console.log('testimonials initialized');
+			// });
+			$('.testimonials-slider').slick({
+				slidesToShow: 1,
+  				slidesToScroll: 1,
+				arrows: false,
+				autoplay: true
+			});
 		},
 		_trustPilot: function(data, slick, firstCall) {
-			console.log(firstCall);
+
 			var content = data;
 			var slick = slick;
-			console.log('called');
-			console.log(data);
-			console.log(slick);
-			if (content == 'undefined') {
-				var content = data;
-			}
-			if (slick == 'undefined' || slick == null) {
-				slick = true;
-			}
 
-			console.log(content);
-			console.log(slick);
 			var limit = 0;
 			limit = 10;
 			for (var i=0; i <= limit; i++) {
-				//console.log(i);
-				// console.log(content.reviews[i].text);
-				// console.log(content.reviews[i].consumer.displayName);
-				// console.log(content.reviews[i].stars);
-				// Manipulate dom with response data then triigger this._testimonials()
-				console.log(i);
-				// console.log(limit);
-				$('.testimonials-slider').prepend('<div><p>'+content.reviews[i].text+'</p><span class="from">'+content.reviews[i].consumer.displayName+'</span><span class="trust-pilot-logo"><img src="'+window.location.origin+'/wp-content/themes/angular-spa/images/trust-pilot-logo.png" /></span><img src="'+window.location.origin+'/wp-content/themes/angular-spa/images/trustpilot/'+content.reviews[i].stars+'-stars.png" /></div>');
-				if (i == limit) {
-
-					if(slick) {
-						console.log('done with data');
-						paramountDefault._testimonials();
-					}
-				}
+				if (content.reviews[i].text.length >= 328) { continue; }
+				$('.testimonials-slider').prepend('<div><span class="header">'+content.reviews[i].title+'</span><p>'+content.reviews[i].text+'</p><span class="from">'+content.reviews[i].consumer.displayName+'</span><span class="trust-pilot-logo"><img src="'+window.location.origin+'/wp-content/themes/angular-spa/images/trust-pilot-logo.png" /></span><img src="'+window.location.origin+'/wp-content/themes/angular-spa/images/trustpilot/'+content.reviews[i].stars+'-stars.png" /></div>');
 			}
+			paramountDefault._testimonials();
 			if(!firstCall) {
-				this._trustPilot = function () {};
+				//this._trustPilot = function () {};
 			}
 		},
 		_trustPilotData : function (data, slick, firstCall) {
 				var data = $.ajax({
-					url: 'https://api.trustpilot.com/v1/business-units/561c33b50000ff00058444fb/reviews',
+					url: 'https://api.trustpilot.com/v1/business-units/561c33b50000ff00058444fb/reviews?orderBy=createdat.desc',
 					dataType: 'json',
 					data: {apikey: 'SVAl5Wgi4QDCQEfomKRUq1PHC1OuZVgm'},
 					global: false,
